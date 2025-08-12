0a7c8f78406a0b25b0a1c13421b2141459b0170b
LiorKirshner <liorkk7@gmail.com>
2025-08-12 18:09:34 +0300

    ex1 done

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
-import React, { useState } from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import { CATEGORIES, CHARMS_DATA, POTIONS_DATA } from './Constants';
-import HogwartNavbar from './components/Header/HogwartNavbar';
-import Entities from './components/Entities/Entities';
-import EntityDescription from './components/Entities/EntityDescription';
-import About from './components/About'
+import React, { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Home from "./components/Home";
+import { CATEGORIES, CHARMS_DATA, POTIONS_DATA } from "./Constants";
+import HogwartNavbar from "./components/Header/HogwartNavbar";
+import Entities from "./components/Entities/Entities";
+import EntityDescription from "./components/Entities/EntityDescription";
+import About from "./components/About";
 
 const App = (props) => {
   const [categories, setCategories] = useState(CATEGORIES);
   const [potions, setPotions] = useState(POTIONS_DATA);
   const [charms, setCharms] = useState(CHARMS_DATA);
   const STATE_MAP = {
-    potions:potions,
-    charms:charms
-  }
+    potions: potions,
+    charms: charms,
+  };
 
   const getCategoryData = (category) => {
     return STATE_MAP[category];
-  }
+  };
 
   return (
-    <div>
-        <div className="App">
-          <HogwartNavbar />
-        </div>
-        {/* add routes and route here */}
-    </div>
-
+    <BrowserRouter>
+      <div className="App">
+        <HogwartNavbar />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home categories={categories} />} />
+        <Route
+          path="/wiki/:category"
+          element={<Entities getCategoryData={getCategoryData} />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
--- src/components/Categories/CategoryList.js
+++ src/components/Categories/CategoryList.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import CategoryCard from './CategoryCard';
+import React from "react";
+import { Link } from "react-router-dom";
+import CategoryCard from "./CategoryCard";
 
 const CategoryList = ({ categories }) => {
   return (
     <div className="category-list">
-        {/* add link to each of these */}
-        <CategoryCard id="potions" categoryData={categories[0]} />
-        <CategoryCard id="charms" categoryData={categories[1]} />
-
-      {categories.slice(2).map(category => (
-          <CategoryCard categoryData={category} />
-        ))}
+      {categories.map((category) => (
+        <Link
+          key={category.id || category.name}
+          to={`/wiki/${category.routeLink}`}
+        >
+          <CategoryCard id={category.routeLink} categoryData={category} />
+        </Link>
+      ))}
     </div>
   );
-}
+};
 
 export default CategoryList;
